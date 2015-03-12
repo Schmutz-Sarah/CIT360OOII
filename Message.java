ee50625a41e943dfec73b87da8900730652ced34
Schmutz-Sarah <sch11033@byui.edu>
2015-03-11 19:02:42 -0600

    Create Message.java

---
+++ Message.java
@@ -0,0 +1,35 @@
+public class Message {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	private int ID;
+	private String firstName;
+	private String lastName;
+	
+	public int getID() {
+		return ID;
+	}
+	
+	public void setID(int id) {
+		ID = id;
+	}
+	
+	public String getFristName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+}
